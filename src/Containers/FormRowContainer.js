2af6ba69fb43f847ca901e06f19640e83c750e6a
sruti <sruti@openaq.org>
2020-03-22 14:45:26 -0700

    Update yes/no dropdown to toggle buttons

--- src/Containers/FormRowContainer.js
+++ src/Containers/FormRowContainer.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react'
+import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
+import ToggleButton from 'react-bootstrap/ToggleButton'
 
 export class FormRowContainer extends Component {
 
-    handleChange = (e) => {
-        this.props.handleOption(this.props.rowData.state, e.target.value)
+    handleChange = (val) => {
+        this.props.handleOption(this.props.rowData.state, val)
     }
 
     render() {
-        let {rowData, currentState, index} = this.props
+        let { rowData, currentState, index } = this.props
 
-        if (rowData.state == "age"){
+        if (rowData.state == "age") {
             console.log("CURRENT STATE OF", rowData.title, currentState);
         }
 
         return (
-            <section> 
-            <h2>{index}. {rowData.title}</h2>
-                <select onChange={this.handleChange} value={currentState}> 
+            <section>
+                <p>{index}. {rowData.title}</p>
+
+                <ToggleButtonGroup type="radio" name="studyOptions" value={currentState} onChange={this.handleChange}>
+                    <ToggleButton value={"Yes"}>Yes</ToggleButton>
+                    <ToggleButton value={"No"}>No</ToggleButton>
+                    <ToggleButton value={"Unknown"}>Unknown</ToggleButton>
+                </ToggleButtonGroup >
+                {/* <select onChange={this.handleChange} value={currentState}>
                     <option value="Yes">Yes </option>
                     <option value="No"> No </option>
                     <option value="Unknown"> Unknown </option>
-                </select> 
+                </select> */}
             </section>
         )
     }
--- src/Containers/MainContainer.js
+++ src/Containers/MainContainer.js
@@ -28,30 +28,30 @@ export class MainContainer extends Component {
     // }
 
     displaySorted = () => {
-        let {sortValue} = this.state
+        let { sortValue } = this.state
         // sorted from the lowest to the highest value
 
-        if (sortValue === "Mortality"){
+        if (sortValue === "Mortality") {
             return [
-                {"state": "age", "title": "Age (>/=65yr)"},
-                {"state": "diabetes", "title": "Diabetes"},
-                {"state": "hypertension", "title": "Hypertension"},
-                {"state": "rr24", "title": "RR>24"},
-                {"state": "cad", "title": "CAD"},
-                {"state": "covidExposure", "title": "COVID-19 Exposure"} //doesn't have a value in excel
+                { "state": "age", "title": "Age (>/=65yr)" },
+                { "state": "diabetes", "title": "Diabetes" },
+                { "state": "hypertension", "title": "Hypertension" },
+                { "state": "rr24", "title": "RR>24" },
+                { "state": "cad", "title": "CAD" },
+                { "state": "covidExposure", "title": "COVID-19 Exposure" } //doesn't have a value in excel
             ]
-        } else if (sortValue === "ARDS"){
+        } else if (sortValue === "ARDS") {
             return [
-                {"state": "temp", "title": "Temp. >/= 39C"},
-                {"state": "hypertension", "title": "Hypertension"},
-                {"state": "diabetes", "title": "Diabetes"},
-                {"state": "age", "title": "Age (>/=65yr)"},
-                {"state": "rr24", "title": "RR>24"}, //excel value unclear
+                { "state": "temp", "title": "Temp. >/= 39C" },
+                { "state": "hypertension", "title": "Hypertension" },
+                { "state": "diabetes", "title": "Diabetes" },
+                { "state": "age", "title": "Age (>/=65yr)" },
+                { "state": "rr24", "title": "RR>24" }, //excel value unclear
             ]
         } else {
             return [
-                {"state": "temp", "title": "Temp. >/= 39C"},
-                {"state": "age", "title": "Age (>/=65yr)"},
+                { "state": "temp", "title": "Temp. >/= 39C" },
+                { "state": "age", "title": "Age (>/=65yr)" },
             ]
         }
     }
@@ -59,8 +59,8 @@ export class MainContainer extends Component {
     render() {
         return (
             <main>
-                MAIN CONT
-                <SortContainer handleSort={this.handleSort}/>
+                {/* MAIN CONT */}
+                <SortContainer handleSort={this.handleSort} />
                 <FormContainer data={this.displaySorted()} />
             </main>
         )
