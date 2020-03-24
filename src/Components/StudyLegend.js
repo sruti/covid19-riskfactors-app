82d3fc362eed5b91006e26a3624243dce418e999
sruti <sruti@openaq.org>
2020-03-24 07:21:44 -0700

    Update legend with ratio definitions

--- src/Components/StudyLegend.js
+++ src/Components/StudyLegend.js
@@ -2,24 +2,26 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 
 
-export default function StudyLegend({ study: { info, citation, comment }}) {
-    
+export default function StudyLegend({ study: { info, citation, comment } }) {
+
     return (
         <Container fluid
-                style={{
-                    backgroundColor: "#007bff40",
-                    margin: "2% 0 1% 0",
-                    textAlign: "left",
-                    padding: "1% 1% 1% 1%"
-                }}
-            >
-            <p> <strong>Source: </strong>{ citation } </p>
+            style={{
+                backgroundColor: "#007bff40",
+                margin: "2% 0 1% 0",
+                textAlign: "left",
+                padding: "1% 1% 1% 1%"
+            }}
+        >
+            <p> <strong>OR: </strong>Odds Ratio</p>
+            <p> <strong>HR: </strong>Hazard Ratio (Bivariate Cox Regression Analysis)</p><br></br>
+            <p> <strong>Source: </strong>{citation} </p>
             {
-                comment 
-                ? <p> <strong>Note: </strong>{ comment } </p>
-                : null
+                comment
+                    ? <p> <strong>Note: </strong>{comment} </p>
+                    : null
             }
-            <span> { info } </span>
+            <span> {info} </span>
         </Container>
     )
 }
