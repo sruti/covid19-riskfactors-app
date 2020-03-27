f2dbaa4d7a5e1f661d0b088273e129228baf288b
sruti <msrutiv@gmail.com>
2020-03-27 16:16:31 -0700

    Update procal, lympho. Remove Neutrophilia.

--- src/data.js
+++ src/data.js
@@ -13,14 +13,13 @@ const mortality = [
     {
         "advanced": [
             { "stateName": "sofa", "title": "↑ SOFA", "ratio": "OR 5.65", "ratioTitle": "5.56x per unit↑" },
-            { "stateName": "neutro", "title": "Neutrophilia", "ratio": "?" }, //?Leukocytosis >10 (OR 6.60*)
             { "stateName": "lympho", "title": "Lymphopenia (Abs <0.8)", "ratio": "p <0.05", "ratioTitle": "p <0.05" },
             { "stateName": "ddimer", "title": "D-Dimer >1 ug/ml", "ratio": "OR 18.42", "ratioTitle": "18.42x per unit↑" },
             { "stateName": "ferritin", "title": "Ferritin >300 ug/L", "ratio": "OR 4.62*", "ratioTitle": "*4.62x per unit↑" },
             { "stateName": "ldh", "title": "LDH >245", "ratio": "OR 45.43*", "ratioTitle": "*45.43x per unit↑" },
             { "stateName": "plateles", "title": "Low Platelets", "ratio": "p <0.05", "ratioTitle": "p <0.05" },
             { "stateName": "pt", "title": "PT ≥ 16", "ratio": "OR 4.62*", "ratioTitle": "*4.62x per unit↑" },
-            { "stateName": "procal", "title": "↑ Procalcitonin <0.1", "ratio": "OR 13.75*", "ratioTitle": "*13.75x per unit↑" },
+            { "stateName": "procal", "title": "↑ Procalcitonin", "ratio": "OR 13.75*", "ratioTitle": "*13.75x per unit↑" },
             { "stateName": "cr", "title": "Cr >133 umol/L", "ratio": "OR 4.39*", "ratioTitle": "*4.39x per unit↑" },
             { "stateName": "hstrop", "title": "hs-Trop >28 pg/ml", "ratio": "OR 80.07*", "ratioTitle": "*80.07x per unit↑" },
             { "stateName": "hscrp", "title": "hs-CRP >5 mg/L", "ratio": "Excluded", "ratioTitle": "Excluded" },
@@ -48,14 +47,13 @@ const ards = [
     {
         "advanced": [
             { "stateName": "sofa", "title": "↑ SOFA", "ratio": "Excluded", "ratioTitle": "Excluded" },
-            { "stateName": "neutro", "title": "Neutrophilia", "ratio": "HR 1.14", "ratioTitle": "14% ↑" },
-            { "stateName": "lympho", "title": "Lymphopenia (Abs <0.8)", "ratio": "HR 0.37", "ratioTitle": "63% ↓" },
+            { "stateName": "lympho", "title": "Lymphopenia", "ratio": "HR 0.37", "ratioTitle": "63% ↓" },
             { "stateName": "ddimer", "title": "D-Dimer >1 ug/ml", "ratio": "HR 1.03", "ratioTitle": "3% ↑" },
             { "stateName": "ferritin", "title": "Ferritin >300 ug/L", "ratio": "HR 3.53", "ratioTitle": "3.53x chance" },
             { "stateName": "ldh", "title": "LDH >245", "ratio": "HR 1.61", "ratioTitle": "61% ↑" },
             { "stateName": "plateles", "title": "Low Platelets", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
             { "stateName": "pt", "title": "PT ≥ 16", "ratio": "HR 1.56", "ratioTitle": "56% ↑" },
-            { "stateName": "procal", "title": "↑ Procalcitonin <0.1", "ratio": "Excluded", "ratioTitle": "Excluded" },
+            { "stateName": "procal", "title": "↑ Procalcitonin", "ratio": "Excluded", "ratioTitle": "Excluded" },
             { "stateName": "cr", "title": "Cr >133 umol/L", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
             { "stateName": "hstrop", "title": "hs-Trop >28 pg/ml", "ratio": "Excluded", "ratioTitle": "Excluded" },
             { "stateName": "hscrp", "title": "hs-CRP >5 mg/L", "ratio": "HR 4.81", "ratioTitle": "4.81x chance" },
@@ -83,14 +81,13 @@ const ardsDeath = [
     {
         "advanced": [
             { "stateName": "sofa", "title": "↑ SOFA", "ratio": "Excluded", "ratioTitle": "Excluded" },
-            { "stateName": "neutro", "title": "Neutrophilia", "ratio": "HR 1.08", "ratioTitle": "8% ↑" },
-            { "stateName": "lympho", "title": "Lymphopenia (Abs <0.8)", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
+            { "stateName": "lympho", "title": "Lymphopenia", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
             { "stateName": "ddimer", "title": "D-Dimer >1 ug/ml", "ratio": "HR 1.02", "ratioTitle": "2% ↑" },
             { "stateName": "ferritin", "title": "Ferritin >300 ug/L", "ratio": "HR 3.53", "ratioTitle": "6.17x chance" },
             { "stateName": "ldh", "title": "LDH >245", "ratio": "HR 1.30", "ratioTitle": "30% ↑" },
             { "stateName": "plateles", "title": "Low Platelets", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
             { "stateName": "pt", "title": "PT ≥ 16", "ratio": "HR 1.08", "ratioTitle": "8% ↑" },
-            { "stateName": "procal", "title": "↑ Procalcitonin <0.1", "ratio": "Excluded", "ratioTitle": "Excluded" },
+            { "stateName": "procal", "title": "↑ Procalcitonin", "ratio": "Excluded", "ratioTitle": "Excluded" },
             { "stateName": "cr", "title": "Cr >133 umol/L", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
             { "stateName": "hstrop", "title": "hs-Trop >28 pg/ml", "ratio": "Excluded", "ratioTitle": "Excluded" },
             { "stateName": "hscrp", "title": "hs-CRP >5 mg/L", "ratio": "Insignificant", "ratioTitle": "Insignificant" },
