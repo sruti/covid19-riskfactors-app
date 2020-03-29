b8d496aa48740fb8c449035de30bd3dfacf3b871
sylwiavargas <sylwia.vargas@gmail.com>
2020-03-29 16:56:59 -0400

    typo

--- src/Components/Counter.js
+++ src/Components/Counter.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import ProgressBar from 'react-bootstrap/ProgressBar'
-import { Jumbotron } from 'react-bootstrap';
-
 
 export default function Counter({ counter }) {
     console.log(counter);
     return (
-        <div class="risk-counter">
+        <div className="risk-counter">
             <h5><strong>Risk Factor Count:</strong>  {counter}</h5>
             <ProgressBar variant="danger" now={(counter / 23) * 100}
             />
