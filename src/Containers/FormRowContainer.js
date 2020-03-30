12580f9f6be7105e2827003ee71813482968a609
sylwiavargas <sylwia.vargas@gmail.com>
2020-03-29 21:10:59 -0400

    code hooked up

--- src/Containers/FormRowContainer.js
+++ src/Containers/FormRowContainer.js
@@ -5,44 +5,42 @@ import Col from 'react-bootstrap/Col'
 import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
 import ToggleButton from 'react-bootstrap/ToggleButton'
 import { FormRowContainer as FormRow } from './FormRowContainer';
-// import { rr24YesDataObject, rr24NoDataObject, alt40YesDataObject, alt40NoDataObject, astYesDataObject, astNoDataObject } from '../data.js'
-
 
 export class FormRowContainer extends Component {
 
     // this state exists here only for when this component is recursively a parent
-    state = {
-        alt40YesState: false,
-        alt40NoState: false,
-        astYesState: false,
-        astNoState: false,
-        rr24YesState: false,
-        rr24NoState: false,
-    }
+    // state = {
+    //     alt40YesState: false,
+    //     alt40NoState: false,
+    //     astYesState: false,
+    //     astNoState: false,
+    //     rr24YesState: false,
+    //     rr24NoState: false,
+    // }
 
     // in recursion, this component becomes the parent and function is here as well as in FormCont so we can pass it
-    handleClick = (stateName) => {
-        this.setState(prevState => {
-            return {
-                [stateName]: !prevState[stateName]
-            }
-        })
-    }
+    // handleClick = (stateName) => {
+    //     this.setState(prevState => {
+    //         return {
+    //             [stateName]: !prevState[stateName]
+    //         }
+    //     })
+    // }
 
-    handleChange = (val) => {
-        let points;
+    handleClick = (val) => {
+        // let points;
 
-        if (val[0] === false) {
-            points = this.props.rowData.points
-        } else {
-            points = this.props.rowData.points * -1
-        }
+        // if (val[0] === false) {
+        //     points = this.props.rowData.points
+        // } else {
+        //     points = this.props.rowData.points * -1
+        // }
         this.props.handleClick(this.props.rowData.stateName)
     }
 
     render() {
         let { rowData, currentParentState, exceptions, handleClick } = this.props
-        // console.log(exceptions);
+        console.log(currentParentState);
         let bgcolor = "white";
 
         if (currentParentState) {
@@ -93,7 +91,7 @@ export class FormRowContainer extends Component {
                 type="checkbox"
                 name="studyOptions"
                 value={currentParentState}
-                onChange={this.handleChange}
+                onChange={this.props.handleClick}
             >
                 <ToggleButton
                     variant={currentParentState ? "dark" : "outline-dark"}
@@ -103,7 +101,33 @@ export class FormRowContainer extends Component {
         </Col>
         </Row>
         <Row>
-            {/* Here will be the conditional rendering happeniing -- code below the class */}
+            {/* if you click on the button, create two new rows*/}
+ {currentParentState
+    ?
+    <>
+        {/* depending on the row, conditionally render the correct data */}
+        < FormRow
+            handleChange={this.handleChange}
+            handleClick={this.handleClick}
+            handleCounter={this.props.handleCounter}
+            rowData={rowData.stateName.includes("dyspnea")
+                ? exceptions[0]
+                : (rowData.stateName.includes("alt40") ? exceptions[2] : exceptions[4])}
+            currentParentState={rowData.stateName.includes("dyspnea")
+                ? this.state.rr24YesState
+                : (rowData.stateName.includes("alt40") ? this.state.alt40YesState : this.state.astYesState)} />
+        < FormRow
+            handleChange={this.handleChange}
+            handleClick={this.handleClick}
+            handleCounter={this.props.handleCounter}
+            rowData={rowData.stateName.includes("dyspnea")
+                ? exceptions[1]
+                : (rowData.stateName.includes("alt40") ? exceptions[3] : exceptions[5])}
+            currentParentState={rowData.stateName.includes("dyspnea")
+                ? this.state.rr24NoState
+                : (rowData.stateName.includes("alt40") ? this.state.alt40NoState : this.state.astNoState)} />
+    </>
+    : null}
         </Row>
             </Container>
             : /////////////////// this is for the non-exceptions /////////////////
@@ -131,7 +155,7 @@ export class FormRowContainer extends Component {
                 type="checkbox"
                 name="studyOptions"
                 value={currentParentState}
-                onChange={this.handleChange}
+                onChange={this.props.handleClick}
             >
                 <ToggleButton
                     variant={currentParentState ? "dark" : "outline-dark"}
@@ -148,33 +172,3 @@ export class FormRowContainer extends Component {
 }
 
 export default FormRowContainer
-
-// TO BE PASTED AND DEBUGGED BETWEEN 105 and 107
-
-//  {/* if you click on the button, create two new rows*/}
-//  {currentParentState
-//     ?
-//     <>
-//         {/* depending on the row, conditionally render the correct data */}
-//         < FormRow
-//             handleChange={this.handleChange}
-//             handleClick={this.handleClick}
-//             handleCounter={this.props.handleCounter}
-//             rowData={rowData.stateName.includes("dyspnea")
-//                 ? rr24YesDataObject
-//                 : (rowData.stateName.includes("alt40") ? alt40YesDataObject : astYesDataObject)}
-//             currentParentState={rowData.stateName.includes("dyspnea")
-//                 ? this.state.rr24YesState
-//                 : (rowData.stateName.includes("alt40") ? this.state.alt40YesState : this.state.astYesState)} />
-//         < FormRow
-//             handleChange={this.handleChange}
-//             handleClick={this.handleClick}
-//             handleCounter={this.props.handleCounter}
-//             rowData={rowData.stateName.includes("dyspnea")
-//                 ? rr24NoDataObject
-//                 : (rowData.stateName.includes("alt40") ? alt40NoDataObject : astNoDataObject)}
-//             currentParentState={rowData.stateName.includes("dyspnea")
-//                 ? this.state.rr24NoState
-//                 : (rowData.stateName.includes("alt40") ? this.state.alt40NoState : this.state.astNoState)} />
-//     </>
-//     : null}
