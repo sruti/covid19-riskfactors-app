6fba0cfa649e042c4f7efd7f8e13d22671a689e9
sruti <msrutiv@gmail.com>
2020-03-30 21:21:09 -0700

    Add footer with contact

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import MainContainer from './Containers/MainContainer';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <section className="App">
-      <Header/>
-      <MainContainer/>
+      <Header />
+      <MainContainer />
+      <Footer />
     </section>
   );
 }
---
+++ src/Components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Navbar } from 'react-bootstrap'
+
+export default function Footer() {
+    return (
+        <footer class="credits">
+            <div>
+                Built by <a href="https://github.com/sylwiavargas">@sylwiavargas</a> and <a href="https://github.com/sruti">@sruti</a>
+                <br></br> Contact: <a href="mailto:tn2290@cumc.columbia.edu">tn2290@cumc.columbia.edu</a>
+            </div>
+        </footer>
+    )
+}
